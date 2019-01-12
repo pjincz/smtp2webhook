54ffd629d20e7410f54b277df8b6e1790ea01187
Chizhong Jin <pjincz@gmail.com>
2019-01-12 08:33:12 +0800

    change mail size limitation

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   upstream: 'http://d-api.cubinote.com/home/printmail',
   ak: 'e8562670b6c840688f34044309b0fdd0',
   domain: 'jcz.onl',
-  maxSize: 10 * 1024 * 1024, // 10MB
+  maxSize: 1400 * 1024, // 1.4MB, because upstream can only receive 2M at most with base64 encoding
   debug: true,
 };
